335d119e7e608de7648708f57e6040a9df1b3ad4
Sebastian Thiems <sthiems@sieber-riedl.de>
2019-02-24 20:03:21 +0100

    Add filter function

--- src/main/java/de/cw/cwcocktailapi/controller/v1/controller/CocktailGetControllerV1.java
+++ src/main/java/de/cw/cwcocktailapi/controller/v1/controller/CocktailGetControllerV1.java
@@ -2,12 +2,15 @@ package de.cw.cwcocktailapi.controller.v1.controller;
 
 import de.cw.cwcocktailapi.controller.v1.dto.GetCocktailDto;
 import de.cw.cwcocktailapi.controller.v1.mapper.CocktailMapper;
+import de.cw.cwcocktailapi.domain.IngredientCategory;
 import de.cw.cwcocktailapi.service.CocktailService;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +21,18 @@ public class CocktailGetControllerV1 {
   private CocktailService cocktailService;
 
   @RequestMapping(value = "/cocktails")
-  public List<GetCocktailDto> getCocktails() throws IOException {
-    return CocktailMapper.INSTANCE.cocktailsToGetCocktailDtos(cocktailService.getCocktails());
+  public List<GetCocktailDto> getCocktails(
+      @RequestParam(name = "category", required = false) List<IngredientCategory> categories,
+      @RequestParam(required = false, defaultValue = "0") Integer missingCategories)
+      throws IOException {
+    return CocktailMapper.INSTANCE
+        .cocktailsToGetCocktailDtos(cocktailService.getCocktails(categories, missingCategories));
+  }
+
+  @RequestMapping(value = "/cocktails/{cocktailId}")
+  public GetCocktailDto getCocktail(@PathVariable Long cocktailId) throws IOException {
+    return CocktailMapper.INSTANCE
+        .cocktailToGetCocktailDto(cocktailService.getCocktail(cocktailId));
   }
 
 }
--- src/main/java/de/cw/cwcocktailapi/service/CocktailService.java
+++ src/main/java/de/cw/cwcocktailapi/service/CocktailService.java
@@ -4,10 +4,15 @@ import de.cw.cwcocktailapi.dao.CocktailDao;
 import de.cw.cwcocktailapi.domain.Cocktail;
 import de.cw.cwcocktailapi.domain.CocktailIngredient;
 import de.cw.cwcocktailapi.domain.Ingredient;
+import de.cw.cwcocktailapi.domain.IngredientCategory;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,7 +24,7 @@ public class CocktailService {
   @Autowired
   private IngredientService ingredientService;
 
-  private List<Cocktail> setAndGetCocktailIngredients(List<Cocktail> cocktails,
+  private void setAndGetCocktailIngredients(List<Cocktail> cocktails,
       List<Ingredient> ingredients) {
     for (Cocktail cocktail : cocktails) {
       for (CocktailIngredient cocktailIngredient : cocktail.getIngredients()) {
@@ -31,10 +36,9 @@ public class CocktailService {
         }
       }
     }
-    return cocktails;
   }
 
-  private List<Cocktail> setAndGetAlternativeCocktailIngredients(List<Cocktail> cocktails,
+  private void setAndGetAlternativeCocktailIngredients(List<Cocktail> cocktails,
       List<Ingredient> ingredients) {
     for (Cocktail cocktail : cocktails) {
       for (CocktailIngredient cocktailIngredient : cocktail.getIngredients()) {
@@ -44,15 +48,50 @@ public class CocktailService {
         ).collect(Collectors.toList()));
       }
     }
+  }
+
+  private List<Cocktail> filterCocktailsByCategories(List<Cocktail> cocktails,
+      List<IngredientCategory> filterCategories, Integer missingCategories) {
+    Set<Cocktail> filteredCocktails = new HashSet<>();
+    for (Cocktail cocktail : cocktails) {
+      Integer countFitCategories = 0;
+      for (CocktailIngredient ingredient : cocktail.getIngredients()) {
+        for (IngredientCategory category : ingredient.getIngredient().getCategories()) {
+          for (IngredientCategory filterCategory : filterCategories) {
+            if (filterCategory.equals(category)) {
+              countFitCategories += 1;
+            }
+          }
+        }
+      }
+      if (countFitCategories.equals(filterCategories.size() - missingCategories)) {
+        filteredCocktails.add(cocktail);
+      }
+    }
+    return new ArrayList<>(filteredCocktails);
+  }
+
+  public List<Cocktail> getCocktails(List<IngredientCategory> categories,
+      Integer missingCategories) throws IOException {
+    return filterCocktailsByCategories(getCocktails(), categories, missingCategories);
+  }
+
+  private List<Cocktail> getCocktails() throws IOException {
+    List<Cocktail> cocktails = cocktailDao.getCocktails();
+    List<Ingredient> ingredients = ingredientService.getIngredients();
+    setAndGetCocktailIngredients(cocktails, ingredients);
+    setAndGetAlternativeCocktailIngredients(cocktails, ingredients);
     return cocktails;
   }
 
-  public List<Cocktail> getCocktails() throws IOException {
-    List<Cocktail> cocktails = cocktailDao.getCocktails();
-    List<Ingredient> ingredients = ingredientService.getIngredients();
-    cocktails = setAndGetCocktailIngredients(cocktails, ingredients);
-    cocktails = setAndGetAlternativeCocktailIngredients(cocktails, ingredients);
-    return cocktails;
+  public Cocktail getCocktail(Long cocktailId) throws IOException {
+    List<Cocktail> cocktails = getCocktails();
+    for (Cocktail cocktail : cocktails) {
+      if (cocktail.getId().equals(cocktailId)) {
+        return cocktail;
+      }
+    }
+    throw new ResourceNotFoundException();
   }
 
 }
